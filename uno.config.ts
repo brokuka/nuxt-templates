800757502558a62c6a318c7be7881ace8247ab98
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-06-30 00:47:44 +0400

    chore: init template

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig, presetUno, transformerVariantGroup } from 'unocss'
+import { defineConfig, presetUno, presetWind, transformerVariantGroup } from 'unocss'
+import presetAnimations from 'unocss-preset-animations'
+import { presetShadcn } from 'unocss-preset-shadcn'
 
 export default defineConfig({
-  presets: [
-    presetUno(),
-  ],
-  transformers: [
-    transformerVariantGroup(),
-  ],
+	presets: [
+		presetUno(),
+		presetAnimations(),
+		presetShadcn({
+			color: 'blue',
+		}),
+	],
+	content: {
+		pipeline: {
+			include: [
+				// the default
+				/\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+				// include js/ts files
+				'components/**/*.{js,ts}',
+			],
+		},
+	},
+	transformers: [
+		transformerVariantGroup(),
+	],
 })
